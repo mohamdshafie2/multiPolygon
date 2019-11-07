3471beb6faf778dab32c269b81322cd63809b6eb
M-S-A <mohamdshafie@gmail.com>
2019-11-06 20:33:17 -0600

    initial maven

---
+++ src/main/java/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+
+// Sample URL from google docs
+//READ MORE AT     // https://developers.google.com/maps/documentation/urls/guide#map-action
+//    https://www.google.com/maps/@?api=1&map_action=map&center=-33.712206,150.311941&zoom=12&basemap=terrain
+
+
+
+
+
+
+}
